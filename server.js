0c680191fab955841a16b4f45cc6b5c316046c7b
heyimchester <heyimchester@gmail.com>
2017-09-04 01:08:43 -0700

    test API endpoints

--- server.js
+++ server.js
@@ -30,3 +30,92 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
     app.use(express.static(__dirname + '/dist'));
 });
 
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error":message});
+}
+
+
+// API Routes 
+
+/* "/api/notes/:name"
+ *  GET: find all notes related to passed in name 
+ *  POST: create a new note for passed in name
+ */ 
+app.get("/api/notes/:name", function(req, res) {
+    var name = req.params.name; // Get name of user from post request to differentiate
+
+    //.find() returns a cursors(pointer to result but times out), use toArray
+    db.collection(name + 's_notes').find({}).toArray(function(err, docs) {
+        if(err) {
+            handleError(res, err.message, "Failed to get notes.")
+        } else {
+            res.status(200).json(docs);
+        }
+    });
+
+});
+
+app.post("/api/notes/:name", function(req, res) {
+    var name = req.params.name;
+    var newNote = req.body;
+
+    if(!req.body.content) {
+        handleError(res, "Invalid user input", "Must provide content", 400);
+    }
+
+    db.collection(name + 's_notes').insertOne(newNote, function(err, doc) {
+        if(err){
+            handleError(res, err.message, "Failed to create a new note.");
+        } else {
+            res.status(201).json(doc.ops[0]);
+        }
+    });
+
+});
+
+
+/* "/api/notes/:name/:id"
+ *  GET: find note by id
+ *  PUT: update note by id
+ *  DELETE: delete note by id
+ */
+app.get("/api/notes/:name/:id", function(req, res) {
+    var name = req.paramas.name;
+
+    db.collection(name + 's_notes').findOne({_id: new ObjectID(req.params.id)}, function(err, doc) {
+        if(err) {
+            handleError(res, err.message, "Failed to get note");
+        } else {
+            res.status(200).json(doc);
+        }
+    });
+});
+
+app.put("/api/notes/:name/:id", function(req, res) {
+    var name = req.params.name;
+    var updateNote = req.body;
+    delete updateNote._id;
+
+    db.collection(name + 's_notes').updateOne({_id: new ObjectID(req.params.id)}, updateNote, function(err, doc) {
+        if(err) {
+            handleError(res, err.message, "Failed to update note")
+        } else {
+            updateNote._id = req.params.id;
+            res.status(200).json(updateNote);
+        }
+    });
+    
+})
+
+app.delete("/api/notes/:name/:id", function(req, res) {
+    var name = req.params.name;
+
+    db.collection(name + 's_ntoes').deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+        if (err) {
+            handleError(res, err.message, "Failed to delete note");
+        } else{
+            res.status(200).json(req.params.id);
+        }
+    });
+});
